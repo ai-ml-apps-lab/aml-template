edaf85c898019ee3a4251bdfca5de1eef57e9cd3
ML Applications Lab <251872832+ai-ml-apps-lab@users.noreply.github.com>
2026-03-10 21:32:06 +0100

    Update run_config.py

--- code/train/run_config.py
+++ code/train/run_config.py
@@ -1,18 +1,23 @@
-from azureml.core import ComputeTarget
+from azureml.core.compute import ComputeTarget
 from azureml.train.estimator import Estimator
 
 def main(workspace):
 
-    compute_target = ComputeTarget(workspace=workspace, name="githubcluster")
+    compute_target = ComputeTarget(
+        workspace=workspace,
+        name="githubcluster"
+    )
+
+    script_params = {
+        "--kernel": "linear",
+        "--penalty": 1.0
+    }
 
     estimator = Estimator(
         source_directory="code/train",
         entry_script="train.py",
+        script_params=script_params,
         compute_target=compute_target,
-        script_params={
-            "--kernel": "linear",
-            "--penalty": 1.0
-        },
         pip_packages=[
             "azureml-dataprep[pandas,fuse]",
             "scikit-learn",
